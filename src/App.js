b2281ad96e0b1d68fd60a660a67981cef65ec44b
camilo perez <cami20032017@gmail.com>
2024-02-18 21:12:27 -0500

    feat: initial blog version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import Presentation from './components/Presentation'
+import CreateCard from './components/CreateCard'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Camilo Dev
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Presentation/>
+      <div className='activities'>
+        <CreateCard
+          imageName='actividad1.jpg'
+          cardTitle='Actividad 1'
+          cardText='Linea de tiempo'
+          urlEvidence="https://unidadestecno-my.sharepoint.com/:i:/g/personal/caperezquintanilla_uts_edu_co/ESXD9-K7jztNvdTBuuHQf7cBKCBppUic5M8r14gM5jtbag?e=PUtPOB"
+        />
+      </div>
+      
+
     </div>
   );
 }
