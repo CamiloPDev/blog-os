7c571a4c690c53a5953e6c3a37b193ac4b610cfe
CamiloDev <cami20032017@gmail.com>
2024-03-02 20:55:03 -0500

    feat: delete bootstrap and add personal style

--- src/components/cards/activity/ActivityCard.jsx
+++ src/components/cards/activity/ActivityCard.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 
 export default function ActivityCard({imageName, cardTitle, cardText, urlEvidence}){
     return(
-        <div className='card activity-card' style={{width: '18rem'}}>
-            <img src={`${process.env.PUBLIC_URL}/card-images/${imageName}`} className='card-img-top' alt='...'/>
-            <div className='card-body'>
-                <h5 className='card-title'>{cardTitle}</h5>
-                <p className="card-text">{cardText}</p>
-                <a href={`${urlEvidence}`} className="btn btn-primary" target="_blank" rel="noopener noreferrer">Entrar</a>
+        <div className='container-activity' style={{width: '18rem'}}>
+            <img src={`${process.env.PUBLIC_URL}/card-images/${imageName}`} alt='...'/>
+            <div className='activity-text'>
+                <h5 className='activity-title'>{cardTitle}</h5>
+                <p className="activity-text">{cardText}</p>
+                <a href={`${urlEvidence}`} className="activity-button" target="_blank" rel="noopener noreferrer">Entrar</a>
             </div>
         </div>
     )
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css';
 import Home from './pages/home/Home';
 import Cut1 from './pages/cuts/cut1/Cut1';
