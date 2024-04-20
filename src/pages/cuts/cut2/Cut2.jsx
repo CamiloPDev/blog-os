7df92cd64313bca72566a599fb51b28bf0728b08
CamiloPDev <99335525+CamiloPDev@users.noreply.github.com>
2024-04-19 21:25:13 -0500

    feat: update component and new activity (#6)

--- src/pages/cuts/cut2/Cut2.jsx
+++ src/pages/cuts/cut2/Cut2.jsx
@@ -6,7 +6,16 @@ export default function Cut2() {
     return (
         <>
             <Presentation activeLink={true}/>
-            <Accordion title='Bloque de control de procesos (BCP)' pdfName='Bloque de control del proceso.pdf'/>
+            <Accordion 
+                title='Bloque de control de procesos (BCP)' 
+                type='pdf' 
+                pdfName='Bloque de control del proceso.pdf'
+            />
+            <Accordion 
+                title='Mapa Mental' 
+                type='image' 
+                imageName='corte2_mapa_mental_memoria.jpeg'
+            />
         </>
     );
 }
