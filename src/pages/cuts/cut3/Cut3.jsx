81893c2985b4cffa3f20ff54bcc37fafb1420e64
CamiloDev <cami20032017@gmail.com>
2024-05-28 21:47:47 -0500

    feat: activities cut 3

--- src/pages/cuts/cut3/Cut3.jsx
+++ src/pages/cuts/cut3/Cut3.jsx
@@ -1,10 +1,25 @@
 import Presentation from '../../../components/presentation/Presentation';
-
+import Accordion from '../../../components/accordion/Accordion';
 
 export default function Cut3() {
     return (
         <>
             <Presentation activeLink={true}/>
+            <Accordion 
+                title='IPV4 / IPV6' 
+                type='pdf' 
+                pdfName='IPV4eIPV6.pdf'
+            />
+            <Accordion 
+                title='Protocolos de red' 
+                type='pdf' 
+                pdfName='Protocolos de red.pdf'
+            />
+            <Accordion 
+                title='Partición de discos' 
+                type='pdf' 
+                pdfName='Partición de discos.pdf'
+            />
         </>
     );
 }
